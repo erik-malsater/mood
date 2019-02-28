7dfd76b1d9b20b219480931a6cfcb38687e8b715
erik <erik.malsater92@gmail.com>
2019-02-28 12:50:43 +0100

    add focus jquery function for main dropdown menu

--- scripts/functions.js
+++ scripts/functions.js
@@ -16,6 +16,38 @@
             }
         });
 
+        ////
+
+        $('#dropdown_search_form_field').focus(function(){
+            if(!$(this).hasClass('dropdown_search_form_field')){
+                $(this).toggleClass('header_search_form_field_filled');
+                var x = $('#dropdown_search_form_button_image').attr('src');
+                if(x === 'images/search_icon_black.png'){
+                    $('#dropdown_search_form_button_image').attr('src', 'images/search_icon_orange.png');
+                } else{
+                    $('#dropdown_search_form_button_image').attr('src', 'images/search_icon_black.png');
+                }
+                //$('#dropdown_search_form_button').toggleClass('header_search_form_button_filled');
+            }
+        });
+        
+        $('#dropdown_search_form_field').blur(function()
+        {
+            if( $(this).val().length === 0 ) {
+                $(this).toggleClass('header_search_form_field_filled');
+                var x = $('#dropdown_search_form_button_image').attr('src');
+                if(x === 'images/search_icon_black.png'){
+                    $('#dropdown_search_form_button_image').attr('src', 'images/search_icon_orange.png');
+                } else{
+                    $('#dropdown_search_form_button_image').attr('src', 'images/search_icon_black.png');
+                }
+                
+                //$('#dropdown_search_form_button').toggleClass('header_search_form_button_filled');
+            }
+        });
+
+        ///
+
         $('#hamburger_button').click(function(){
             $(this).toggleClass('open');
             $('#mobile_dropdown_item_container').slideToggle(200);
