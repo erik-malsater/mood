387360d982902c3cf3308c17c461acc25afcd988
erik <erik.malsater92@gmail.com>
2019-03-13 15:15:36 +0100

    Fix indentation and profile work feed links

--- includes/creators_work_feed_card.php
+++ includes/creators_work_feed_card.php
@@ -9,11 +9,11 @@ $post = new Post($connection);
 $fetched_posts = $post->fetchCreatorsWork($_GET['profile']);
 foreach($fetched_posts as $single_post){
     ?>
-            <div class="work_feed_art_frame">
-                <a href="work.php?post=<?=$single_post['id']?>" aria-label="Go to page for this artwork">
-                    <img src="<?=$single_post['image']?>" alt="<?=$single_post['name']?>">
-                </a>
-            </div>
+            <a href="work.php?post=<?=$single_post['id']?>" aria-label="Go to page for this artwork">
+                <div class="work_feed_art_frame">
+                        <img src="<?=$single_post['image']?>" alt="<?=$single_post['name']?>">
+                </div>
+            </a>
     <?php
 }
 
